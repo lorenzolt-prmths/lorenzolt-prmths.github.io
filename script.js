5d79b558595b94b023228f31c8693b77b0661545
lorenzolt-prmths <lorenzolt@me.com>
2026-03-29 20:57:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 function toggleCategory(categoryId) {
+    // Ocultar todas las listas
     const lists = document.querySelectorAll('.projects-list');
     lists.forEach(list => {
         list.style.display = 'none';
     });
 
+    // Mostrar la lista seleccionada
     const selected = document.getElementById(categoryId);
     selected.style.display = 'block';
 
-    window.scrollTo({
-        top: selected.offsetTop - 80,
+    // Scroll hacia la lista
+    selected.scrollIntoView({
         behavior: 'smooth'
     });
 }
