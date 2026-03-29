8ea8025a3aff8999fa0d7f8a674a162cd54d7d4d
lorenzolt-prmths <lorenzolt@me.com>
2026-03-29 21:06:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 function toggleCategory(categoryId) {
     const lists = document.querySelectorAll('.projects-list');
 
-    // Ocultar todas
+    // Ocultar todas las listas
     lists.forEach(list => {
         list.style.display = 'none';
     });
 
-    // Mostrar la seleccionada
+    // Mostrar la lista seleccionada
     const selected = document.getElementById(categoryId);
     selected.style.display = 'block';
 
-    // Esperar un poco para que el div aparezca antes del scroll
+    // Scroll suave hacia los proyectos
     setTimeout(() => {
-        selected.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
+        window.scrollTo({
+            top: selected.offsetTop - 80,
+            behavior: 'smooth'
         });
     }, 100);
 }
